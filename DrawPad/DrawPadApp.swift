0b1d2af31c73fc526e701f2c4cd683a5c65f02de
rajeshk256 <koyi.rajesh06@gmail.com>
2025-11-07 09:56:00 +0530

    Initial Commit

---
+++ DrawPad/DrawPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawPadApp.swift
+//  DrawPad
+//
+//  Created by Rajesh on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
